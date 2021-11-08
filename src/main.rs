82db406fe96beaf90af70d5fcfcb83f2e538a11b
PhuNH <1079742+PhuNH@users.noreply.github.com>
2021-11-08 14:45:01 +0100

    Codes 1 to 7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,198 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::TryInto;
+use std::fs;
+
+use synacor_vm::SynacorVm;
+
+mod synacor_vm;
+
+pub fn read_input_u16(path: &str) -> Vec<u16> {
+    let bin_input = fs::read(path).unwrap();
+    bin_input.chunks(2)
+        .map(|c| u16::from_le_bytes(c.try_into().unwrap()))
+        .collect()
 }
+
+fn main() {
+    let bin_input = read_input_u16("input/challenge.bin");
+    let mut vm = SynacorVm::new(bin_input);
+    let prepared = r#"doorway
+north
+north
+bridge
+continue
+down
+east
+take empty lantern
+west
+west
+passage
+ladder
+west
+south
+north
+take can
+use can
+use lantern
+west
+ladder
+darkness
+continue
+west
+west
+west
+west
+north
+take red coin
+north
+east
+take concave coin
+down
+take corroded coin
+up
+west
+west
+take blue coin
+up
+take shiny coin
+down
+east
+use blue coin
+use red coin
+use shiny coin
+use concave coin
+use corroded coin
+north
+take teleporter
+use teleporter
+take business card
+"#;
+    vm.run(prepared.chars().collect());
+}
+
+/* python z3
+from z3 import *
+slots = [Int("s_%s" % i) for i in range(5)]
+values = [Or(And(slots[i] < 10, slots[i] > 1, slots[i] % 2 == 1), slots[i] == 2) for i in range(5)]
+distinction = Distinct(slots)
+solve(values + [distinction, slots[0] + slots[1] * slots[2] ** 2 + slots[3] ** 3 - slots[4] == 399])
+[s_0 = 9, s_1 = 2, s_2 = 5, s_3 = 7, s_4 = 3]
+*/
+
+/* The strange book
+The cover of this book subtly swirls with colors.  It is titled "A Brief Introduction to Interdimensional Physics".  It reads:
+
+Recent advances in interdimensional physics have produced fascinating
+predictions about the fundamentals of our universe!  For example,
+interdimensional physics seems to predict that the universe is, at its root, a
+purely mathematical construct, and that all events are caused by the
+interactions between eight pockets of energy called "registers".
+Furthermore, it seems that while the lower registers primarily control mundane
+things like sound and light, the highest register (the so-called "eighth
+register") is used to control interdimensional events such as teleportation.
+
+A hypothetical such teleportation device would need to have have exactly two
+destinations.  One destination would be used when the eighth register is at its
+minimum energy level - this would be the default operation assuming the user
+has no way to control the eighth register.  In this situation, the teleporter
+should send the user to a preconfigured safe location as a default.
+
+The second destination, however, is predicted to require a very specific
+energy level in the eighth register.  The teleporter must take great care to
+confirm that this energy level is exactly correct before teleporting its user!
+If it is even slightly off, the user would (probably) arrive at the correct
+location, but would briefly experience anomalies in the fabric of reality
+itself - this is, of course, not recommended.  Any teleporter would need to test
+the energy level in the eighth register and abort teleportation if it is not
+exactly correct.
+
+This required precision implies that the confirmation mechanism would be very
+computationally expensive.  While this would likely not be an issue for large-
+scale teleporters, a hypothetical hand-held teleporter would take billions of
+years to compute the result and confirm that the eighth register is correct.
+
+If you find yourself trapped in an alternate dimension with nothing but a
+hand-held teleporter, you will need to extract the confirmation algorithm,
+reimplement it on more powerful hardware, and optimize it.  This should, at the
+very least, allow you to determine the value of the eighth register which would
+have been accepted by the teleporter's confirmation mechanism.
+
+Then, set the eighth register to this value, activate the teleporter, and
+bypass the confirmation mechanism.  If the eighth register is set correctly, no
+anomalies should be experienced, but beware - if it is set incorrectly, the
+now-bypassed confirmation mechanism will not protect you!
+
+Of course, since teleportation is impossible, this is all totally ridiculous.
+*/
+
+/* confirmation process
+5483: set r0 4
+5486: set r1 1
+5489: call 6027             af()
+                            # confirmation done
+5491: eq r1 r0 6            r1 = 1 if r0 == 6 else 0
+5495: jf r1 5579            if r1 != 0:
+5498: push r0                 ...
+5500: push r1
+5502: push r2
+5504: set r0 29014
+5507: set r1 1531
+5510: add r2 302 1601
+5514: call 1458
+                            def af:
+6027: jt r0 6035              if r0 == 0:
+6030: add r0 r1 1               r0 = r1 + 1
+6034: ret                       return
+6035: jt r1 6048              if r1 == 0:
+6038: add r0 r0 32767           r0 -= 1
+6042: set r1 r7                 r1 = r7
+6045: call 6027                 af()
+6048: push r0                 push r0
+6050: add r1 r1 32767         r1 -= 1
+6054: call 6027               af()
+6056: set r1 r0               r1 = r0
+6059: pop r0                  pop r0
+6061: add r0 r0 32767         r0 -= 1
+6065: call 6027               af()
+6067: ret                     return
+*/
+
+/* Ackermann function at ip = 6027
+af(0, n) = n + 1
+af(m + 1, 0) = af(m, x) with x = r7
+af(m + 1, n + 1) = af(m, af(m + 1, n))
+
+af(4, 1) = af(3, af(4, 0)) = af(3, af3x)
+af(4, 0) = af(3, x) = af3x
+af(3, n) = af(2, af(3, n-1)) = (x+1) * af(3, n-1) + 2x+1 = (x+1) * ((x+1) * af(3, n-2) + 2x+1) + 2x+1
+         = (x+1)^2 * af(3, n-2) + (x+1+1)(2x+1) = (x+1)^2 * ((x+1) * af(3, n-3) + 2x+1) + (x+1+1)(2x+1)
+         = (x+1)^3 * af(3, n-3) + ((x+1)^2 + x+1 + 1)(2x+1) = ...
+         = (x+1)^n * af(3, 0) + (2x+1)((x+1)^(n-1) + ... + x+1 + 1)
+         = (x+1)^(n+2) + x * (x+1)^n + ((x+1)^(n-1) + ... + x+1 + 1)(x+1 + x)
+         = (x+1)^(n+2) + x * ((x+1)^n + (x+1)^(n-1) + ... + x+1 + 1)
+                       +      (x+1)^n + (x+1)^(n-1) + ... + x+1 + 1 - 1
+         = (x+1)^(n+2) + (x+1)^(n+1) + (x+1)^n + ... + (x+1)^2 + (x+1) + 1 - 2
+         = (x+1)^(n+2) + (x+1)^(n+1) + (x+1)^n + ... + (x+1)^2 + x // use this
+    with x != 0:
+         = ((x+1)^(n+3) - 1) / x - 2 // this cannot be used due to lost accuracy during type conversion
+    with x == 1:
+         = 2^(n+3) - 3
+    with x == 0:
+         = 1 + n
+af(3, 0) = af(2, x) = (x+1)^2 + x
+af(2, n) = af(1, af(2, n-1)) = x+1 + af(2, n-1) = 2(x+1) + af(2, n-2) = ... = n(x+1) + af(2, 0)
+         = n(x+1) + x+1 + x = (n+1)(x+1) + x
+af(2, 0) = af(1, x) = x+1 + x
+af(1, n) = af(0, af(1, n-1)) = af(1, n-1) + 1 = af(1, n-2) + 2 = ... = af(1, 0) + n = x+1 + n
+af(1, 0) = af(0, x) = x+1
+*/
+
+/*
+oHVlEiuRDDqk - from arch-spec
+SaFPTyYYPxtc - welcome before self-test
+xHUCoNHlSgmn - self-test completion
+ycDPlkIuhlbC - tablet
+frOgQzZlguet - found oil
+buMeVvBwqgHJ - after using teleporter with r7 = 0
+uQWfNPkCwlXV - after using teleporter with r7 = 25734
+
+*/
---
+++ src/synacor_vm.rs
@@ -0,0 +1,291 @@
+use std::collections::VecDeque;
+use std::io;
+
+#[derive(Clone, Debug)]
+pub struct SynacorVm {
+    memory: Vec<u16>,
+    registers: Vec<u16>,
+    stack: Vec<u16>,
+    ip: usize
+}
+
+const LITERAL: u16 = 32767;
+const INVALID: u16 = 32776;
+
+fn show_reg(x: u16) -> String {
+    assert!(x > LITERAL && x < INVALID);
+    format!("r{}", x - LITERAL - 1)
+}
+
+fn show_val(x: u16) -> String {
+    assert!(x < INVALID);
+    if x <= LITERAL { x.to_string() }
+    else { show_reg(x) }
+}
+
+fn ackermann_3n(r7: u16, n: u32) -> u16 {
+    // (x+1)^(n+2) + (x+1)^(n+1) + (x+1)^n + ... + (x+1)^2 + x
+    let mut result = 0;
+    let mut exp = n + 2;
+    loop {
+        result = (result as u32 + (r7 + 1).wrapping_pow(exp) as u32) as u16 & LITERAL;
+        exp -= 1;
+        if exp == 1 { break; }
+    }
+    result = (result as u32 + r7 as u32) as u16 & LITERAL;
+    // println!("r7: {}, n: {}, af3n: {}", r7, n, result);
+    result
+}
+
+fn find_r7() -> u16 {
+    let mut r7 = 0;
+    loop {
+        // println!("Checking {}", r7);
+        let af3x = ackermann_3n(r7, r7 as u32);
+        if ackermann_3n(r7, af3x as u32) == 6 {
+            break;
+        }
+        if r7 == LITERAL { break; }
+        r7 += 1;
+    };
+    r7
+}
+
+impl SynacorVm {
+    pub fn new(program: Vec<u16>) -> SynacorVm {
+        SynacorVm {
+            memory: program,
+            registers: vec![0; 8],
+            stack: Vec::new(),
+            ip: 0
+        }
+    }
+
+    fn val(&self, x: u16) -> u16 {
+        assert!(x < INVALID);
+        if x <= LITERAL { x }
+        else { self.registers[(x - LITERAL) as usize - 1] }
+    }
+
+    fn set_reg(&mut self, x: u16, val: u16) {
+        assert!(x > LITERAL && x < INVALID);
+        self.registers[(x - LITERAL) as usize - 1] = val;
+    }
+
+    pub fn run(&mut self, prepared: Vec<char>) -> u32 {
+        let mut in_buffer: VecDeque<char> = VecDeque::from(prepared);
+        let mut out_buffer = String::new();
+        let mut extracting = false;
+        let mut count = 0;
+        let r7 = 25734; // find_r7();
+        let mut stop = 0;
+
+        let result = loop {
+            let mem_len = self.memory.len();
+            if self.ip >= mem_len {
+                break 1;
+            }
+
+            // skip confirmation process
+            if self.ip == 5489 {
+                self.registers[0] = 6;
+                self.ip = 5491;
+            }
+            // after confirmation process
+            // if self.ip == 5491 {
+            //     extracting = true;
+            // }
+            let a = if self.ip + 1 < mem_len { self.memory[self.ip + 1] } else { 0 };
+            let b = if self.ip + 2 < mem_len { self.memory[self.ip + 2] } else { 0 };
+            let c = if self.ip + 3 < mem_len { self.memory[self.ip + 3] } else { 0 };
+            match self.memory[self.ip] {
+                0 => break 0, // halt
+                1 => { // set a b
+                    self.set_reg(a, self.val(b));
+                    if extracting {
+                        println!("{}: set {} {}", self.ip, show_reg(a), show_val(b));
+                    }
+                    self.ip += 3;
+                }
+                2 => { // push a
+                    self.stack.push(self.val(a));
+                    if extracting {
+                        println!("{}: push {}", self.ip, show_val(a));
+                    }
+                    self.ip += 2;
+                }
+                3 => { // pop a
+                    assert!(!self.stack.is_empty());
+                    let val = self.stack.pop().unwrap();
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: pop {}", self.ip, show_reg(a));
+                    }
+                    self.ip += 2;
+                }
+                4 => { // eq a b c
+                    let val = if self.val(b) == self.val(c) { 1 } else { 0 };
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: eq {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                5 => { // gt a b c
+                    let val = if self.val(b) > self.val(c) { 1 } else { 0 };
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: gt {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                6 => { // jmp a
+                    if extracting {
+                        println!("{}: jmp {}", self.ip, show_val(a));
+                    }
+                    self.ip = self.val(a) as usize;
+                }
+                7 => { // jt a b
+                    if extracting {
+                        println!("{}: jt {} {}", self.ip, show_val(a), show_val(b));
+                    }
+                    if self.val(a) != 0 { self.ip = self.val(b) as usize; }
+                    else { self.ip += 3; }
+                }
+                8 => { // jf a b
+                    if extracting {
+                        println!("{}: jf {} {}", self.ip, show_val(a), show_val(b));
+                    }
+                    if self.val(a) == 0 { self.ip = self.val(b) as usize; }
+                    else { self.ip += 3; }
+                }
+                9 => { // add a b c
+                    let val = (self.val(b) + self.val(c)) & LITERAL;
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: add {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                10 => { // mult a b c
+                    let val = ((self.val(b) as u32 * self.val(c) as u32) & LITERAL as u32) as u16;
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: mult {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                11 => { // mod a b c
+                    let val = self.val(b) % self.val(c);
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: mod {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                12 => { // and a b c
+                    let val = self.val(b) & self.val(c);
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: and {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                13 => { // or a b c
+                    let val = self.val(b) | self.val(c);
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: or {} {} {}", self.ip, show_reg(a), show_val(b), show_val(c));
+                    }
+                    self.ip += 4;
+                }
+                14 => { // not a b
+                    let val = !self.val(b) & LITERAL;
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: not {} {}", self.ip, show_reg(a), show_val(b));
+                    }
+                    self.ip += 3;
+                }
+                15 => { // rmem a b
+                    let val = self.memory[self.val(b) as usize];
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: rmem {} {}", self.ip, show_reg(a), show_val(b));
+                    }
+                    self.ip += 3;
+                }
+                16 => { // wmem a b
+                    let loc = self.val(a);
+                    self.memory[loc as usize] = self.val(b);
+                    if extracting {
+                        println!("{}: wmem {} {}", self.ip, show_val(a), show_val(b));
+                    }
+                    self.ip += 3;
+                }
+                17 => { // call a
+                    self.stack.push(self.ip as u16 + 2);
+                    if extracting {
+                        println!("{}: call {}", self.ip, show_val(a));
+                    }
+                    self.ip = self.val(a) as usize;
+                }
+                18 => { // ret
+                    if extracting {
+                        println!("{}: ret", self.ip);
+                    }
+                    if self.stack.is_empty() { break 0; }
+                    else { self.ip = self.stack.pop().unwrap() as usize; }
+                }
+                19 => { // out a
+                    out_buffer.push((self.val(a) as u8) as char);
+                    // confirmation process
+                    // if out_buffer.ends_with("1 billion years.\"") {
+                    //     extracting = true;
+                    // }
+                    // print!("{}", (self.val(a) as u8) as char);
+                    if extracting {
+                        println!("{}: out {}", self.ip, (self.val(a) as u8) as char);
+                    }
+                    self.ip += 2;
+                }
+                20 => { // in a
+                    print!("{}", out_buffer);
+
+                    if in_buffer.is_empty() {
+                        if stop == 0 {
+                            self.registers[7] = r7;
+                            println!("r7: {}", r7);
+                            in_buffer = "use teleporter\n".chars().collect();
+                            stop += 1;
+                        } else {
+                            let mut temp = String::new();
+                            io::stdin().read_line(&mut temp).unwrap();
+                            in_buffer = temp.chars().collect();
+                        }
+                    }
+                    let val = in_buffer.pop_front().unwrap() as u16;
+                    self.set_reg(a, val);
+                    if extracting {
+                        println!("{}: in {}", self.ip, show_reg(a));
+                    }
+                    self.ip += 2;
+                    out_buffer = String::new();
+                }
+                n => {
+                    if extracting {
+                        println!("{}: noop {}", self.ip, n);
+                    }
+                    self.ip += 1;
+                }
+            }
+            if extracting {
+                count += 1;
+                if count >= 1000 { break 2; }
+            }
+        };
+        println!("{}", out_buffer);
+        println!("result {}", result);
+        result
+    }
+}
